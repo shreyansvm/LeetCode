681cd9ef7216a179fcd3e95735b87a12c888278a
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-08-25 13:48:02 -0700

    reverse a 32-bit integer

---
+++ reverse_integer.py
@@ -0,0 +1,55 @@
+class Solution:
+    """
+    Given a 32-bit signed integer, reverse digits of an integer.
+
+    Example : 1
+        Input: 123
+        Output: 321
+
+    Example : 2
+        Input: -123
+        Output: -321
+
+    Example : 3
+        Input: 120
+        Output: 21
+    """
+    def __init__(self):
+        self.orig_int = None
+        self.orig_int_digits = []
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int (reverse digits of an 32-bit signed integer.)
+        """
+        if x < 0:
+            print('do something')
+            return -self.reverse(-x)
+
+        result = 0
+        while x:
+            result = (result * 10) + (x % 10)
+            # print('result : ', result)
+            x = x / 10
+
+        return result
+
+orig_int = 123
+print(Solution().reverse(orig_int))
+# orig_int = 321
+# print(Solution().reverse(orig_int))
+# orig_int = -123
+# print(Solution().reverse(orig_int))
+# orig_int = -321
+# print(Solution().reverse(orig_int))
+# orig_int = 120
+# print(Solution().reverse(orig_int))
+# orig_int = 1
+# print(Solution().reverse(orig_int))
+# orig_int = 10
+# print(Solution().reverse(orig_int))
+# orig_int = 0
+# print(Solution().reverse(orig_int))
+# orig_int = 100000
+# print(Solution().reverse(orig_int))
